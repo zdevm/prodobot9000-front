dab7b90632940b6f933cb8ca041493e2c2c59f88
Marios Zafeiriou <zafeidev@gmail.com>
2022-08-13 20:11:12 +0300

    refactor menu

--- src/app/modules/menu/menu.component.ts
+++ src/app/modules/menu/menu.component.ts
@@ -1,19 +1,33 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LoginService } from '@modules/login/services/login.service';
+import { User } from '@modules/user/classes/user';
+import { UserService } from '@modules/user/services/user.service';
 import { TitleService } from '@shared/services/title/title.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnDestroy {
+  user: User | null = null;
+  private unsub$ = new Subject<void>();
 
-  constructor(private loginService: LoginService,
+  constructor(private readonly loginService: LoginService,
+              private readonly userService: UserService,
               private readonly titleService: TitleService) { }
 
   ngOnInit(): void {
     this.titleService.setTitle('Prodobot 9000')
+    this.userService.user
+      .pipe(takeUntil(this.unsub$))
+      .subscribe(user => this.user = user);
+  }
+
+  ngOnDestroy(): void {
+    this.unsub$.next();
+    this.unsub$.complete();
   }
 
   onLoginBtn() {
