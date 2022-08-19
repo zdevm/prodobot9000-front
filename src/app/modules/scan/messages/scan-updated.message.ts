8c39f84865cac14b9f9974550fb7930d0f502fb9
Marios Zafeiriou <zafeidev@gmail.com>
2022-08-19 13:58:20 +0300

    fix: add Type decorator

--- src/app/modules/scan/messages/scan-updated.message.ts
+++ src/app/modules/scan/messages/scan-updated.message.ts
@@ -1,8 +1,9 @@
-import { Expose } from "class-transformer";
+import { Expose, Type } from "class-transformer";
 import { Scan } from "../classes/scan";
 
 export class ScanUpdatedMessage {
   @Expose()
+  @Type(() => Scan)
   scan!: Scan;
 
   static getType() {
